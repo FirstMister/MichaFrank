ed8d402ef0bc255ce016be156c56aa1dcb2c38e8
micha <michafrederik@gmail.com>
2023-04-18 19:01:01 +0200

    changed text

--- curvedtext.js
+++ curvedtext.js
@@ -1,8 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var video = document.getElementById('myVideo');
-  var loadingScreen = document.getElementById('loadingScreen');
+window.addEventListener('load', function () {
+  const loadingScreen = document.getElementById('loadingScreen');
+  const video = document.querySelector('video');
+  const progressBar = document.getElementById('progressBar');
 
-  video.addEventListener('loadeddata', function () {
+  function hideLoadingScreen() {
     loadingScreen.style.display = 'none';
+  }
+
+  function updateProgressBar(percentage) {
+    progressBar.style.width = percentage + '%';
+  }
+
+  video.addEventListener('progress', function () {
+    if (video.buffered.length > 0) {
+      const percentage = (video.buffered.end(0) / video.duration) * 100;
+      updateProgressBar(percentage);
+    }
   });
+
+  if (video.readyState >= 3) {
+    hideLoadingScreen();
+  } else {
+    video.addEventListener('canplaythrough', hideLoadingScreen, false);
+  }
 });
